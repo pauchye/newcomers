22dffee973f9f3ce9e6f098d7aa8815ed5a37f85
Olga <olga.a.smi@gmail.com>
2024-09-06 12:12:02 -0400

    Finish tutorial

--- nextjs-newcomers/app/ui/login-form.tsx
+++ nextjs-newcomers/app/ui/login-form.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -6,13 +7,21 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { useActionState } from 'react';
+import { authenticate } from '@/app/lib/actions';
 
-export default function LoginForm() {
+export default function LoginForm({isSignup = false}: {isSignup: boolean}) {
+  const [errorMessage, formAction, 
+    // isPending
+  ] = useActionState(
+   authenticate,
+    undefined,
+  );
   return (
-    <form className="space-y-3">
+    <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Please {isSignup ? 'sign up' : 'log in'} to continue.
         </h1>
         <div className="w-full">
           <div>
@@ -56,10 +65,15 @@ export default function LoginForm() {
           </div>
         </div>
         <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          { isSignup ? 'Sign up' : 'Log in ' } <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
         </div>
       </div>
     </form>
