5ff7af8cb022b7c03a47eb6c02bba8f076c1c7ff
Olga <olga.a.smi@gmail.com>
2024-09-26 23:10:51 -0400

    Fix build

--- nextjs-newcomers/app/ui/events/gallery.tsx
+++ nextjs-newcomers/app/ui/events/gallery.tsx
@@ -7,13 +7,13 @@ import { getGalleryImagesArray } from '../../lib/helpers'
 const GalleryPage = ({ eventName }: {eventName: string}) => {
     const [imageGallery, setImageGallery] = useState<string[] | undefined>()
     useEffect(() => {
-        const galleryMap: Record<string, string[]> = getGalleryImagesArray()
-        getGalleryImagesArray().then(result => {
-        if (result && result[eventName]){
-          setImageGallery(result[eventName])  
-        }
-        }).catch(() => setImageGallery(undefined))
-
+  const getPictures = async () => {
+    const data = await getGalleryImagesArray()
+        if (data && data[eventName]){
+          setImageGallery(data[eventName])  
+        };
+  }
+getPictures()
     }, [])
     return <div className="grid gap-4 sm:grid-cols-1 lg:grid-cols-2 my-6">
         {imageGallery && imageGallery.map((el: string) => <Image  width={400} height={400} alt={'alt'} src={`/images/events/${eventName}/${el}`} key={el} />)}
