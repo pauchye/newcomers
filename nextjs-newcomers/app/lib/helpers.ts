866f090af7112313681604a8189f82b2acd58ad6
Olga <olga.a.smi@gmail.com>
2024-09-26 23:27:00 -0400

    Test

--- nextjs-newcomers/app/lib/helpers.ts
+++ nextjs-newcomers/app/lib/helpers.ts
@@ -1,13 +1,16 @@
 'use server'
 import * as fs from 'fs'
+import path from 'path'
 
 export const getGalleryImagesArray = () => {
-    const galleryFilenames: string[] = fs.readdirSync(`./public/images/events/`)
+    const galDirectoryPath = path.join(process.cwd(), '/public/images/events/');
+    const galleryFilenames: string[] = fs.readdirSync(galDirectoryPath)
     const GALLERY_MAP: Record<string, string[]> = {}
 
 galleryFilenames.forEach(item => {
-    if (fs.lstatSync(`./public/images/events/${item}`).isDirectory() ) {
-        GALLERY_MAP[item] = fs.readdirSync(`./public/images/events/${item}`)
+    const itemPath = path.join(process.cwd(), `./public/images/events/${item}`)
+    if (fs.lstatSync(itemPath).isDirectory() ) {
+        GALLERY_MAP[item] = fs.readdirSync(itemPath)
     }
 })
 return GALLERY_MAP;
