9f9d1cc1a2c9f0a7e8f3aca78933fc04c9ade97f
Olga <olga.a.smi@gmail.com>
2025-01-28 19:26:00 -0500

    Fix timezone

--- nextjs-newcomers/app/dashboard/calendar/page.tsx
+++ nextjs-newcomers/app/dashboard/calendar/page.tsx
@@ -3,7 +3,7 @@ export default async function Page() {
 
   return (
     <main className="h-full ">
-<iframe className="h-full w-full" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&src=aGVsbG9Ad2lsdG9uaWFucy5vcmc&color=%23039BE5" allowFullScreen={true}></iframe>
+<iframe className="h-full w-full" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&src=aGVsbG9Ad2lsdG9uaWFucy5vcmc&color=%23039BE5" allowFullScreen={true}></iframe>
     </main>
   );
 }
